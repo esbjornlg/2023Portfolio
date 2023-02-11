f7a6740d437705c30cf9e86c4153bdbaebed8598
esbjornlg <esbjornlystergolawski@gmail.com>
2023-02-11 14:00:15 +0100

    Articlescroll initial effects not working fix, Netlify routing fix
    Added netlify.toml to fix paths not "/" generating 404 error.
    Effects in articlescroller now applies directly on load, without having to scroll first.

--- src/components/ArticleScroller.jsx
+++ src/components/ArticleScroller.jsx
@@ -6,19 +6,22 @@ const ArticleScroller = ({Articles, Language}) => {
   const ArticleScroller = useRef();
 
   const handleScroll = () => {
+    if (!ArticleScroller.current)
+      return;
     for (let i = 0; i < ArticleScroller.current.children.length; i++) {
       const completion = styling.getElemScrollCompletion(ArticleScroller.current.children[i], ArticleScroller.current);
       ArticleScroller.current.children[i].style.filter = "blur(" + (1 - completion)*3 + "px)";
       ArticleScroller.current.children[i].style.opacity = completion + 0.4;
     }
   }
+  handleScroll();
 
   return (
     <div ref={ArticleScroller} name="articleScroller" onScroll={handleScroll} className="flexColumn borderBox" style={{width: "100%", maxHeight: "430px", scrollSnapType: "y mandatory", overflowY: "scroll"}}>
       {Articles.map((article, index) => (
         <div key={"Article" + index} className="flexRow borderBox article" style={{justifyContent: "space-evenly", alignItems: "center", width: "100%", minHeight: "400px", padding: "20px", scrollSnapAlign: "start", marginBottom: "10px"}}>
           <div className="flexColumn" style={{justifyContent: "center", width: "55%", maxHeight: "100%"}}>
-              <h3 style={{fontSize: "30px", marginRight: "10px"}}>{article.title[Language.lang]} <p style={{fontSize: "20px"}}>{article.year}</p></h3>
+              <h3 style={{fontSize: "30px", marginRight: "10px", overflowWrap: "break-word", hyphens: "auto"}}>{article.title[Language.lang]} <p style={{fontSize: "20px"}}>{article.year}</p></h3>
             <div className="borderBox article" style={{padding: "10px 20px 20px 0px", borderWidth: "0px", overflowY: "scroll"}}>
                 <p>{article.p[Language.lang]}</p>
             </div>
