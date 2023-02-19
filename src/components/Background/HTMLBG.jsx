cf98e15b49065b1fbec9666123e54d1fc5d8ee02
Esbjörnlg <esbjornlystergolawski@gmail.com>
2023-02-19 22:25:07 +0100

    Reverted previous performance trickledowns, fixed small device rendering
    Reverted all previous performance-commits and changed condition for rendering navbar & header's HTML only when it exists (not on small screens)

--- src/components/Background/HTMLBG.jsx
+++ src/components/Background/HTMLBG.jsx
@@ -9,16 +9,16 @@ const HTMLBG = ({Language, navTrigger, scrollEvent, Links}) => {
 
     const getElemTree = (arr, element) => {
         arr.push({nodeInfo: {nodeName: element.nodeName.toLowerCase(), className: element.className, textContent: element.textContent}, children: []});
-        // for (let i= 0; i < element.children.length; i++)
-        //     arr[arr.length - 1]["children"].push(getElemTree(arr[arr.length-1]["children"], element.children[i]));
+        for (let i= 0; i < element.children.length; i++)
+            arr[arr.length - 1]["children"].push(getElemTree(arr[arr.length-1]["children"], element.children[i]));
     }
 
     const makeHTMLElemTree = (elemObj, index) => {
-        return (index < 1 ? (<div key={index} className={"bgHTMLContent " + (elemObj.children.length ? "" : "flexRow")} style={{marginLeft: "40px"}}>
+        return (<div key={index} className={"bgHTMLContent " + (elemObj.children.length ? "" : "flexRow")} style={{marginLeft: "40px"}}>
                     <HTMLElem key={-1} CompName={elemObj["nodeInfo"]["nodeName"]} ClassName={elemObj["nodeInfo"]["className"]} CompParams={[]} TextContent={elemObj["nodeInfo"]["textContent"]}/>
                         {elemObj.children.map((e, i) => { if (e) return makeHTMLElemTree(e, i)})}
                     <h3 key={-2} className="bgHTMLOff">{"</"}<span style={{color: "var(--htmlComp)"}}>{elemObj["nodeInfo"]["nodeName"]}</span>{">"}</h3>
-                </div>) : <div key={-1 * index}></div>)
+                </div>)
     }
 
     const [BGHTML, setBGHTML] = useState(<></>);
@@ -30,7 +30,7 @@ const HTMLBG = ({Language, navTrigger, scrollEvent, Links}) => {
         let bgHTMLArr = [];
         getElemTree(bgHTMLArr, document.getElementById(Links[linkIndex]));
         setBGHTML(makeHTMLElemTree(bgHTMLArr[0], 0));
-        if (!headerHTML) {
+        if (!headerHTML && window.innerWidth > 1170) {
             let HeaderHTMLArr = [];
             getElemTree(HeaderHTMLArr, document.getElementById("mainPageHeader"));
             setHeaderHTML(makeHTMLElemTree(HeaderHTMLArr[0], 0));
@@ -57,7 +57,7 @@ const HTMLBG = ({Language, navTrigger, scrollEvent, Links}) => {
 
     return (
         <div id="HTMLBG" className="HTMLBG">
-            {(window.innerHeight > 850) ? headerHTML : <></>}
+            {(window.innerWidth > 1170) ? headerHTML : <></>}
             <div ref={bgCompSwitch}>
                 <HTMLElem CompName="About" CompParams={["Language", "navTrigger"]} isClosing={true}/>
                 {pageIndex == 0 ? BGHTML : <></>}
