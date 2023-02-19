37cf4640c07697b6a9637e13a17d4466f745ed48
Esbjörnlg <esbjornlystergolawski@gmail.com>
2023-02-19 22:03:01 +0100

    Reduced HTMLBG elements to improve performance

--- src/components/Background/HTMLBG.jsx
+++ src/components/Background/HTMLBG.jsx
@@ -14,11 +14,11 @@ const HTMLBG = ({Language, navTrigger, scrollEvent, Links}) => {
     }
 
     const makeHTMLElemTree = (elemObj, index) => {
-        return (<div key={index} className={"bgHTMLContent " + (elemObj.children.length ? "" : "flexRow")} style={{marginLeft: "40px"}}>
+        return (index < 2 ? (<div key={index} className={"bgHTMLContent " + (elemObj.children.length ? "" : "flexRow")} style={{marginLeft: "40px"}}>
                     <HTMLElem CompName={elemObj["nodeInfo"]["nodeName"]} ClassName={elemObj["nodeInfo"]["className"]} CompParams={[]} TextContent={elemObj["nodeInfo"]["textContent"]}/>
                         {elemObj.children.map((e, i) => { if (e) return makeHTMLElemTree(e, i)})}
                     <h3 className="bgHTMLOff">{"</"}<span style={{color: "var(--htmlComp)"}}>{elemObj["nodeInfo"]["nodeName"]}</span>{">"}</h3>
-                </div>)
+                </div>) : <></>)
     }
 
     const [BGHTML, setBGHTML] = useState(<></>);
